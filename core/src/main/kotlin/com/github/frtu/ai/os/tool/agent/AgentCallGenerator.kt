e017e3d054f5f03833467e5c14bf5ab0538eeb96
Tu, Fred <frederic.tu@gmail.com>
2023-12-18 01:10:15 +0800

    feat: allow to skip when return type instruction when returning Void

--- core/src/main/kotlin/com/github/frtu/ai/os/tool/agent/AgentCallGenerator.kt
+++ core/src/main/kotlin/com/github/frtu/ai/os/tool/agent/AgentCallGenerator.kt
@@ -13,24 +13,29 @@ object AgentCallGenerator {
         append(owningClass.getPersona())
         append("\n\n")
         append(functionToCall.getAction())
-        append("\n\n")
-        append(
-            """
-        The output should be formatted as a JSON instance that conforms to the JSON schema below.
 
-        As an example, for the schema {"properties": {"foo": {"title": "Foo", "description": "a list of strings", "type": "array", "items": {"type": "string"}}}, "required": ["foo"]}
-        the object {"foo": ["bar", "baz"]} is a well-formatted instance of the schema. The object {"properties": {"foo": ["bar", "baz"]}} is not well-formatted.
+        val returnClass = functionToCall.returnType.jvmErasure.java
+        if (returnClass != Void::class.java) {
+            append("\n\n")
+            append(
+                """
+                The output should be formatted as a JSON instance that conforms to the JSON schema below.
+        
+                As an example, for the schema {"properties": {"foo": {"title": "Foo", "description": "a list of strings", "type": "array", "items": {"type": "string"}}}, "required": ["foo"]}
+                the object {"foo": ["bar", "baz"]} is a well-formatted instance of the schema. The object {"properties": {"foo": ["bar", "baz"]}} is not well-formatted.
+        
+                Here is the output schema:
+                ```
+                """.trimIndent()
+            )
+            append(generateJsonSchema(returnClass))
+            append(
+                """
+                ```
+                """.trimIndent()
+            )
+        }
 
-        Here is the output schema:
-        ```
-        """.trimIndent()
-        )
-        append(generateJsonSchema(functionToCall.returnType.jvmErasure.java))
-        append(
-            """
-        ```
-        """.trimIndent()
-        )
     }
 }
 
--- core/src/test/kotlin/com/github/frtu/ai/os/tool/agent/AgentCallGeneratorTest.kt
+++ core/src/test/kotlin/com/github/frtu/ai/os/tool/agent/AgentCallGeneratorTest.kt
@@ -3,13 +3,15 @@ package com.github.frtu.ai.os.tool.agent
 import com.github.frtu.ai.agents.travel.ValidationAgent
 import com.github.frtu.ai.os.tool.agent.AgentCallGenerator.generateSystemPrompt
 import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.string.shouldNotContain
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 
 class AgentCallGeneratorTest {
 
     @Test
-    fun `test createAgentFunction`() {
+    fun `test createAgentFunction with return type`() {
         //--------------------------------------
         // 1. Init
         //--------------------------------------
@@ -27,6 +29,30 @@ class AgentCallGeneratorTest {
         //--------------------------------------
         with(result) {
             shouldNotBeNull()
+            result shouldContain "Here is the output schema"
+        }
+    }
+
+    @Test
+    fun `test createAgentFunction with no return type`() {
+        //--------------------------------------
+        // 1. Init
+        //--------------------------------------
+        val owningClass = ValidationAgent::class
+        val functionToCall = ValidationAgent::proposeItinerary
+
+        //--------------------------------------
+        // 2. Execute
+        //--------------------------------------
+        val result = generateSystemPrompt(functionToCall, owningClass)
+        logger.debug("result:$result")
+
+        //--------------------------------------
+        // 3. Validate
+        //--------------------------------------
+        with(result) {
+            shouldNotBeNull()
+            result shouldNotContain "Here is the output schema"
         }
     }
 
