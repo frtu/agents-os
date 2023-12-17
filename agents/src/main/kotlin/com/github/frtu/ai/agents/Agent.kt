a043ae9834018e4e6fcd2320806b0088842314a0
Tu, Fred <frederic.tu@gmail.com>
2023-12-18 00:12:59 +0800

    feat: adding ability to read function annotation

--- agents/src/main/kotlin/com/github/frtu/ai/agents/Agent.kt
+++ agents/src/main/kotlin/com/github/frtu/ai/agents/Agent.kt
@@ -1,7 +1,9 @@
 package com.github.frtu.ai.agents
 
+import com.github.frtu.ai.agents.annotation.Action
 import com.github.frtu.ai.agents.annotation.Persona
 import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
 import kotlin.reflect.full.findAnnotations
 
 @Persona(
@@ -15,5 +17,11 @@ open class Agent
 fun KClass<out Agent>.getPersona(): String {
     val findAnnotations = this.findAnnotations<Persona>()
     findAnnotations.ifEmpty { throw IllegalArgumentException("You must annotate your agent class:[${this}] with @Persona(prompt)") }
+    return findAnnotations.first().prompt.trim()
+}
+
+fun KFunction<*>.getAction(): String {
+    val findAnnotations = this.findAnnotations<Action>()
+    findAnnotations.ifEmpty { throw IllegalArgumentException("You must annotate your function:[${this}] with @Action(prompt)") }
     return findAnnotations.first().prompt.trim()
 }
--- agents/src/test/kotlin/com/github/frtu/ai/agents/AgentKtTest.kt
+++ agents/src/test/kotlin/com/github/frtu/ai/agents/AgentKtTest.kt
@@ -1,11 +1,13 @@
 package com.github.frtu.ai.agents
 
 import com.github.frtu.ai.agents.travel.TravelAgent
+import com.github.frtu.ai.agents.travel.ValidationAgent
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 
@@ -55,7 +57,53 @@ class AgentKtTest {
         }
     }
 
-    class RandomAgent : Agent()
+    @Test
+    fun `getAction for a function tagged with @Action`() {
+        //--------------------------------------
+        // 1. Init
+        //--------------------------------------
+        val function = ValidationAgent::validate
+
+        //--------------------------------------
+        // 2. Execute
+        //--------------------------------------
+        val result = function.getAction()
+        logger.debug("result:$result")
+
+        //--------------------------------------
+        // 3. Validate
+        //--------------------------------------
+        with(result) {
+            shouldNotBeNull()
+            result shouldContain "plan_is_valid"
+        }
+    }
+
+    @Test
+    fun `getAction for a function without annotation`() {
+        //--------------------------------------
+        // 1. Init
+        //--------------------------------------
+        val function = RandomAgent::withoutAnnotation
+
+        //--------------------------------------
+        // 2. Execute
+        //--------------------------------------
+        val result = shouldThrow<IllegalArgumentException> { function.getAction() }
+        logger.debug("result:$result")
+
+        //--------------------------------------
+        // 3. Validate
+        //--------------------------------------
+        with(result) {
+            shouldNotBeNull()
+            result.message shouldContain function.name
+        }
+    }
+
+    class RandomAgent : Agent() {
+        fun withoutAnnotation(): Nothing = TODO()
+    }
 
     private val logger = LoggerFactory.getLogger(this::class.java)
 }
