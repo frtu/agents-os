10677995f564748f1cff6d76304a7c0e34b79eeb
Tu, Fred <frederic.tu@gmail.com>
2023-12-18 01:00:14 +0800

    feat: use travel agent as example

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 dependencies {
     implementation(project(":core"))
+    implementation(project(":agents"))
 
     implementation("com.kjetland:mbknor-jackson-jsonschema_2.13:1.0.39")
 
--- app/src/main/kotlin/com/github/frtu/ai/agents/os/app/App.kt
+++ app/src/main/kotlin/com/github/frtu/ai/agents/os/app/App.kt
@@ -1,9 +1,11 @@
 package com.github.frtu.ai.agents.os.app
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.github.frtu.ai.agents.travel.ValidationAgent
 import com.github.frtu.ai.os.llm.Chat
 import com.github.frtu.ai.os.memory.Conversation
-import com.github.frtu.ai.os.planning.orchestration.WorkflowGenerator.createWorkflowFunction
+import com.github.frtu.ai.os.planning.orchestration.WorkflowGenerator.createWorkflowDefinition
+import com.github.frtu.ai.os.tool.agent.AgentCallGenerator.generateSystemPrompt
 import com.github.frtu.ai.os.tool.registry
 import com.github.frtu.ai.os.utils.FileLoader.readFileFromClasspath
 import kotlinx.serialization.json.jsonPrimitive
@@ -15,7 +17,7 @@ suspend fun main() {
         "Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous."
 
     val functionRegistry = registry {
-        register(function = createWorkflowFunction(::currentWeather))
+        register(function = createWorkflowDefinition(::currentWeather))
         function(
             name = "get_current_weather", description = "Get the current weather in a given location",
             kFunction2 = ::currentWeather,
@@ -30,19 +32,35 @@ suspend fun main() {
     val chat: Chat = OpenAiChat(
         apiKey = apiKey,
         model = model,
-        functionRegistry = functionRegistry,
+//        functionRegistry = functionRegistry,
         defaultEvaluator = { chatChoices -> chatChoices.first() }
     )
-    val conversation = Conversation(systemDirective) // OR calling recallConversation()
+    val conversation = Conversation(
+        generateSystemPrompt(
+            ValidationAgent::validate,
+            ValidationAgent::class
+        )
+    ) // OR calling recallConversation()
 
     with(conversation) {
 //        val response = chat.sendMessage(user("What's the weather like in Glasgow, Scotland today?"))
-        val response = chat.sendMessage(user("Write me a workflow with operation to call a function 'function1()'"))
+        val response =
+            chat.sendMessage(
+                user(
+                    """
+                    I want to do 2 week trip from Berkeley CA to New York City.
+                    I want to visit national parks and cities with good food.
+                    I want use a rental car and drive for no more than 5 hours on any given day.
+                    """.trimIndent()
+                )
+            )
         println(response)
 
         // Handle response
+        // Sanity check for message structure (contains function call OR message)
         val message = response.message
         message.functionCall?.let { functionCall ->
+            // Should check token before append OR summarizing
             this.addResponse(message)
 
             val functionToCall = functionRegistry.getFunction(functionCall.name)
