f0702efd9ce24f27ed99b3976eddb498d7b5fdcd
Tu, Fred <frederic.tu@gmail.com>
2025-01-01 14:18:36 +0100

    chore: migrate to Spring Boot 3

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
     // Spring
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "2.7.15"
+    id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.6"
 
     // Application
