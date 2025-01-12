fdf561d97aeeaff75b75619574fbcbd6f345df1d
Tu, Fred <frederic.tu@gmail.com>
2025-01-12 23:38:20 +0800

    chore: Allow to print all Agent spring bean during startup

--- service/src/main/kotlin/com/github/frtu/ai/os/service/AssistantApplication.kt
+++ service/src/main/kotlin/com/github/frtu/ai/os/service/AssistantApplication.kt
@@ -1,9 +1,13 @@
 package com.github.frtu.ai.os.service
 
 import com.github.frtu.ai.os.service.intent.config.IntentClassifierConfig
+import com.github.frtu.kotlin.ai.os.llm.agent.AgentExecuter
 import com.github.frtu.kotlin.spring.tool.config.SpringToolAutoConfigs
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 
 @SpringBootApplication
@@ -11,7 +15,16 @@ import org.springframework.context.annotation.Import
     IntentClassifierConfig::class,
     SpringToolAutoConfigs::class,
 )
-class AssistantApplication
+class AssistantApplication {
+    @Bean
+    fun startup(registry: List<AgentExecuter>,): CommandLineRunner = CommandLineRunner {
+        registry.map {
+            logger.info("Detected agent spring bean:${it.id}")
+        }
+    }
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+}
 
 fun main(args: Array<String>) {
     runApplication<AssistantApplication>(*args)
